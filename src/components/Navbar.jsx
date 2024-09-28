c8e5eddd2c18ae3be42a97c4ffab5593717a8882
huzaifa1230 <huzaifairshad173@gmail.com>
2024-09-28 18:31:20 +0500

    fix: comment out navbar hlogo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 // import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
-import hlogo from "../assets/tech/hlogo.jpg";
+// import hlogo from "../assets/tech/hlogo.jpg";
 const Navbar = () => {
   // const [active, setActive] = useState("");
   // const [toggle, setToggle] = useState(false);
